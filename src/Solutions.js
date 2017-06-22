35e4c05a7824a9b9f3383f930f3ba546705270c6
Jay Honnold <jhonnold@mtu.edu>
2017-06-22 13:17:50 -0500

    Lambda expressions are a start

--- src/Solutions.js
+++ src/Solutions.js
@@ -1,34 +1,9 @@
 import React from 'react';
 
-class Solutions extends React.Component{ 
+class Solutions extends React.Component{
     constructor(props) {
         super(props);
-        this.A_hasBeenClicked = this.A_hasBeenClicked.bind(this);
-        this.B_hasBeenClicked = this.B_hasBeenClicked.bind(this);
-        this.C_hasBeenClicked = this.C_hasBeenClicked.bind(this);
-        this.D_hasBeenClicked = this.D_hasBeenClicked.bind(this);
-        this.E_hasBeenClicked = this.E_hasBeenClicked.bind(this);
         this.state = {answerChosen: '0'};
-    }
-
-    A_hasBeenClicked() {
-        this.setState({answerChosen: 'A'});
-    }
-
-    B_hasBeenClicked() {
-        this.setState({answerChosen: 'B'});
-    }
-
-    C_hasBeenClicked() {
-        this.setState({answerChosen: 'C'});
-    }
-
-    D_hasBeenClicked() {
-        this.setState({answerChosen: 'D'});
-    }
-
-    E_hasBeenClicked() {
-        this.setState({answerChosen: 'E'});
     }
 
     render() {
@@ -38,22 +13,22 @@ class Solutions extends React.Component{
             return (
                 <div>
                     <p> </p>
-                    <button onClick={this.A_hasBeenClicked}>A</button>
-                    <button onClick={this.B_hasBeenClicked}>B</button>
-                    <button onClick={this.C_hasBeenClicked}>C</button>
-                    <button onClick={this.D_hasBeenClicked}>D</button>
-                    <button onClick={this.E_hasBeenClicked}>E</button>
+                    <button onClick={() => this.setState({answerChosen: 'A'})}>A</button>
+                    <button onClick={() => this.setState({answerChosen: 'B'})}>B</button>
+                    <button onClick={() => this.setState({answerChosen: 'C'})}>C</button>
+                    <button onClick={() => this.setState({answerChosen: 'D'})}>D</button>
+                    <button onClick={() => this.setState({answerChosen: 'E'})}>E</button>
                 </div>
             );
         } else {
             return (
                 <div>
                     <p>You have selected {answer}</p>
-                    <button onClick={this.A_hasBeenClicked}>A</button>
-                    <button onClick={this.B_hasBeenClicked}>B</button>
-                    <button onClick={this.C_hasBeenClicked}>C</button>
-                    <button onClick={this.D_hasBeenClicked}>D</button>
-                    <button onClick={this.E_hasBeenClicked}>E</button>
+                    <button onClick={() => this.setState({answerChosen: 'A'})}>A</button>
+                    <button onClick={() => this.setState({answerChosen: 'B'})}>B</button>
+                    <button onClick={() => this.setState({answerChosen: 'C'})}>C</button>
+                    <button onClick={() => this.setState({answerChosen: 'D'})}>D</button>
+                    <button onClick={() => this.setState({answerChosen: 'E'})}>E</button>
                 </div>
             );
         }
